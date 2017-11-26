8c9075be26b31512e080c239453ba2fc1cee8bf6
André Ligné <hello@bzf.se>
2017-11-26 14:09:44 +0100

    Add archives to _dict in DirectoryArchive

--- src/directory_archive.cc
+++ src/directory_archive.cc
@@ -2,7 +2,26 @@
 
 #include <boost/filesystem.hpp>
 
+#include <archive.h>
+#include <archive_entry.h>
+
+#include "utils.hh"
 #include "directory_archive.hh"
+
+bool
+is_archive(const char *path) {
+  auto archive = archive_read_new();
+
+  archive_read_support_filter_all(archive);
+  archive_read_support_format_all(archive);
+
+  if (archive_open_and_read_from_path(path, archive, 10240) == ARCHIVE_OK) {
+    archive_read_free(archive);
+    return true;
+  } else {
+    return false;
+  }
+}
 
 DirectoryArchive::DirectoryArchive(const char *directory_path)
   : _directory_path(directory_path) {
@@ -20,5 +39,12 @@ DirectoryArchive::DirectoryArchive(const char *directory_path)
       std::cout << "Filename: " << filename << std::endl;
       std::cout << "Filename with format: " << filename_without_format << std::endl;
       std::cout << "Full path: " << full_path << "\n" << std::endl;
+
+      if (is_archive(full_path.c_str())) {
+        auto my_archive = Archive(full_path);
+        _dict.insert({ filename_without_format, my_archive });
+      }
     }
+
+    std::cout << "DirectoryArchive::_dict.size(): " << _dict.size() << std::endl;
 }
