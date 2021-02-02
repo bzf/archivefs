62e0c15f61fcbb4c13fbccaf28996ff8153ea501
André Ligné <andre@ligne.io>
2021-02-02 07:59:57 +0100

    Use bindgen for generating FFI for libarchive
    Since I goofed up on a signature before, having the computer generate
    them instead seem like the safer bet going forward.

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,45 +1,23 @@
 extern crate libc;
 
-use libc::{size_t, ssize_t};
 use std::ffi::CString;
 use std::os::raw::c_char;
 use std::ptr;
 
 use utils;
 
-pub enum ArchiveEntry {}
-pub enum Archive {}
-
-extern "C" {
-    pub fn archive_read_data(_: *mut Archive, _: *mut c_char, _: size_t) -> ssize_t;
-    pub fn archive_read_data_skip(_: *mut Archive) -> i64;
-
-    pub fn archive_seek_data(_: *mut Archive, _: i64, _: i64) -> i64;
-
-    pub fn archive_read_next_header(_: *mut Archive, _: *mut *mut ArchiveEntry) -> i64;
-
-    pub fn archive_entry_pathname(_: *mut ArchiveEntry) -> *const c_char;
-    pub fn _archive_entry_filetype(_: *mut ArchiveEntry) -> libc::mode_t;
-    pub fn archive_entry_size(_: *mut ArchiveEntry) -> i64;
-
-    pub fn archive_read_new() -> *mut Archive;
-    pub fn archive_read_support_filter_all(_: *mut Archive) -> i64;
-    pub fn archive_read_support_format_all(_: *mut Archive) -> i64;
-
-    pub fn archive_read_open_filenames(
-        archive: *mut Archive,
-        filenames: *const *const c_char,
-        block_size: libc::size_t,
-    ) -> i64;
-
-    pub fn archive_read_free(_: *mut Archive) -> i64;
-}
+pub use {
+    archive, archive_entry, archive_entry_pathname, archive_entry_size, archive_read_data,
+    archive_read_data_skip, archive_read_free, archive_read_new, archive_read_next_header,
+    archive_read_open_filenames, archive_read_support_filter_all, archive_read_support_format_all,
+    archive_seek_data,
+};
 
 pub struct FuseFileInfo {}
 
 pub fn archive_open_and_read_from_path(
     path: &str,
-    archive: *mut Archive,
+    archive: *mut archive,
     buffer_size: libc::size_t,
 ) -> i64 {
     let parts: Vec<String> = utils::get_all_archive_filenames(&path);
@@ -51,9 +29,9 @@ pub fn archive_open_and_read_from_path(
 
     let mut ptr_parts: Vec<*const c_char> = cparts.iter().map(|x| x.as_ptr()).collect();
     ptr_parts.push(ptr::null_mut());
-    let vec: *const *const c_char = ptr_parts.as_ptr();
+    let vec: *mut *const c_char = ptr_parts.as_ptr() as *mut *const c_char;
 
     unsafe {
-        return archive_read_open_filenames(archive, vec, buffer_size);
+        return archive_read_open_filenames(archive, vec, buffer_size as u64) as i64;
     };
 }
